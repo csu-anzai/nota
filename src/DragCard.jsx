1f720778c2e5f407b897900f168449a3b89657c1
Benjamin Leeds <baleeds@ncsu.edu>
2019-08-16 12:48:12 -0400

    remove to be one axis

--- src/DragCard.jsx
+++ src/DragCard.jsx
@@ -4,23 +4,14 @@ import { fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
 import SubscriptionManager from './subscriptionManager';
 import CappedArray from './cappedArray';
-import easing from './easing';
-
-const FRICTION = .99;
-
-const FRAME = 1 / 60;
-
-const BASICALLY_ZERO = 0.01;
 
 const HOME_POINT = {
-  x: 0,
-  y: 0,
+  position: 0,
   gravity: 10,
 };
 
 const AWAY_POINT = {
-  x: 1100,
-  y: 0,
+  position: 500,
   gravity: 10,
 };
 
@@ -28,17 +19,17 @@ const SLIDE_EFFECT = 10;
 
 const APPROX_MAX_DIFFERENCE = 20;
 
-const getWinningRestingPoint = (restingPoints, position, velocity) => {
-  return restingPoints.reduce((target, restingPoint) => {
+const getWinningRestingPoint = (restingPoints, position, velocity) => restingPoints
+  .reduce((target, restingPoint) => {
     const score = getRestingPointScore(restingPoint, position, velocity);
     if (score > target.score) return { score, restingPoint };
 
     return target;
-  }, { score: 0, restingPoint: null });
-}
+  }, { score: 0, restingPoint: null })
+  .restingPoint;
 
-const getRestingPointScore = ({ x, y, gravity }, {x: posX, y: posY }, { velocityX, velocityY}) =>
-  gravity / ( Math.abs(x - (posX + velocityX * SLIDE_EFFECT)) + Math.abs(y - (posY + velocityY * SLIDE_EFFECT)) );
+const getRestingPointScore = ({ position: restingPointPosition, gravity }, position, velocity) =>
+  gravity / Math.abs(restingPointPosition - (position + velocity * SLIDE_EFFECT));
 
 const toTouchEvent = (e) => {
   e.preventDefault();
@@ -51,19 +42,16 @@ const toTouchEvent = (e) => {
 };
 
 const getNextVelocity = (
-  { velocityX, velocityY },
-  { deltaX, deltaY },
+  velocity,
+  delta,
   index
 ) => {
   const factor = (1 / (index + 1));
 
-  return {
-    velocityX: velocityX + (deltaX * factor),
-    velocityY: velocityY + (deltaY * factor),
-  }
+  return velocity + (delta * factor);
 };
 
-const getVelocity = (deltas) => deltas.reduce(getNextVelocity, { velocityX: 0, velocityY: 0 });
+const getVelocity = (deltas) => deltas.reduce(getNextVelocity, 0);
 
 const signsAreDifferent = (a, b) => a * b < 0;
 
@@ -75,13 +63,10 @@ const clamp = (number, bottom, top) => {
   return number;
 }
 
-const getBezierHandle = ({ velocityX, velocityY }, { deltaX, deltaY }) => {
-  const averageDelta = (deltaX + deltaY) / 2
+const getBezierHandle = (velocity, delta) => {
+  const averageVelocityForMove = delta * ANIMATION_TIME;
 
-  const averageVelocityForMove = averageDelta * ANIMATION_TIME;
-  const averageVelocity = (velocityX + velocityY) / 2;
-
-  const relativeMagnitudeOfVelocity = (Math.abs(averageVelocity) - Math.abs(averageVelocityForMove)) / APPROX_MAX_DIFFERENCE; // why 20?;
+  const relativeMagnitudeOfVelocity = (Math.abs(velocity) - Math.abs(averageVelocityForMove)) / APPROX_MAX_DIFFERENCE; // why 20?;
   const clampedRMV = clamp(relativeMagnitudeOfVelocity, -1, 1);
 
   let bezierA = -0.5 * clampedRMV + 0.5; // A = -(1/2)rmv + 0.5
@@ -90,16 +75,16 @@ const getBezierHandle = ({ velocityX, velocityY }, { deltaX, deltaY }) => {
   bezierA = clamp(bezierA, 0, 1); // prevent spilling outside logical range
   bezierB = clamp(bezierB, -1, 1); // prevent spilling outside logical range
 
-  if (signsAreDifferent(averageVelocity, averageVelocityForMove)) { // if moving in the wrong direction
+  if (signsAreDifferent(velocity, averageVelocityForMove)) { // if moving in the wrong direction
     bezierB *= -1; // flip B
   }
 
   console.log({
     bezierA,
     bezierB,
-    averageDelta,
+    delta,
     relativeMagnitudeOfVelocity,
-    averageVelocity,
+    velocity,
     averageVelocityForMove,
   });
 
@@ -110,7 +95,9 @@ class DragCard extends Component {
   constructor(props) {
     super(props);
 
-    this.currentPosition = { x: 0, y: 0 };
+    this.axis = props.axis || 'y';
+
+    this.currentPosition = 0;
     this.lastTouchPosition = null;
     this.deltas = new CappedArray(3);
 
@@ -153,33 +140,28 @@ class DragCard extends Component {
   }
 
   handleTouchMove = (e) => {
-    const { x: screenX, y: screenY } = e;
+    const touchPosition = e[this.axis];
 
     if (this.lastTouchPosition) {
-      const { x, y } = this.lastTouchPosition;
-
-      const delta = {
-        deltaX: screenX - x,
-        deltaY: screenY - y,
-      };
+      const delta = touchPosition - this.lastTouchPosition;
 
       this.move(delta);
 
       this.deltas.push(delta);
     }
 
-    this.lastTouchPosition = { x: screenX, y: screenY };
+    this.lastTouchPosition = touchPosition;
   }
 
   handleTouchEnd = () => {
     const velocity = getVelocity(this.deltas.get());
 
-    const { restingPoint } = getWinningRestingPoint(this.restingPoints, this.lastTouchPosition, velocity);
+    const restingPoint = getWinningRestingPoint(this.restingPoints, this.lastTouchPosition, velocity);
+    if (!restingPoint) return;
 
-    const delta = { 
-      deltaX: restingPoint.x - this.lastTouchPosition.x,
-      deltaY: restingPoint.y - this.lastTouchPosition.y,
-    };
+    const { position: restingPointPosition } = restingPoint;
+
+    const delta = restingPointPosition - this.lastTouchPosition;
 
     const { bezierA, bezierB } = getBezierHandle(velocity, delta);
 
@@ -188,22 +170,17 @@ class DragCard extends Component {
     this.deltas.clear();
     this.lastTouchPosition = null;
 
-    this.updatePosition({ x: restingPoint.x, y: restingPoint.y });
+    this.updatePosition(restingPointPosition);
   }
 
-  move = ({ deltaX, deltaY }) => {
-    const { x: currentX, y: currentY } = this.currentPosition;
-
-    const x = currentX + deltaX;
-    const y = currentY + deltaY;
-
-    this.updatePosition({ x, y });
+  move = (delta) => {
+    this.updatePosition(this.currentPosition + delta);
   }
 
   updatePosition = (pos) => {
     const { current: { style } } = this.card;
 
-    style.transform = `translate(${pos.x}px, ${pos.y}px)`;
+    style.transform = `translate${this.axis.toUpperCase()}(${pos}px)`;
 
     this.currentPosition = pos;
   }
